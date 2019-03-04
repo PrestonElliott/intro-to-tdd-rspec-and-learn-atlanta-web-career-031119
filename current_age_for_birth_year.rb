34f156aa88b9e85ed798a0c2da437a56499b83ac
Preston Elliott <preston.elliott24@gmail.com>
2019-03-04 21:12:55 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -2,4 +2,4 @@ def current_age_for_birth_year(year, year_of_birth)
   return age_of_person = (year - year_of_birth)
 end
 
-current_age_for_birth_year(2019 - 1984)
+current_age_for_birth_year(2019, 1984)
